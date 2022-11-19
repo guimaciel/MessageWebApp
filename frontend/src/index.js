549a407ae3b6c828883e7ca130ca23cb699d9d53
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-11-18 23:09:42 -0800

    Adjusts to add Login and Api calls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
+import ReactDOM from 'react-dom/client'; // <- This import is only for React version 18
 
-import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
+// import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
 
 import './index.css';
 
@@ -10,19 +10,23 @@ import App from './App';
 
 // const root = //ReactDOM.createRoot(document.getElementById('root'));
 
-const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
+// const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
 
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-  <React.StrictMode>
 
-    <App />
+// root.render(
 
-  </React.StrictMode>,
+//   <React.StrictMode>
 
-  root
+//     <App />
 
-);
+//   </React.StrictMode>,
+
+//   root
+
+// );
 
 
 
